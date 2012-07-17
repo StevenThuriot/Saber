f59b66f42fedb3703b38cba7d97edc4143b1e852
Steven Thuriot <steven@thuriot.be>
2012-07-17 20:22:02 +0200

    Updated version info to reflect the new name.

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -13,6 +13,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyInformationalVersion("0.5.0.0")]
-[assembly: AssemblyFileVersion("0.5.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

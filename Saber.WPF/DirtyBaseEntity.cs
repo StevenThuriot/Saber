e8ed28c1c3986e39cb26a5eba5055559ba300be1
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2015-04-04 12:28:32 +0200

    Update LICENSE

--- Saber.WPF/DirtyBaseEntity.cs
+++ Saber.WPF/DirtyBaseEntity.cs
@@ -1,20 +1,3 @@
-#region License
-// 
-//  Copyright 2012 Steven Thuriot
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// 
-#endregion
 using System;
 using System.ComponentModel;
 using System.Runtime.Serialization;
@@ -164,7 +147,7 @@ namespace Saber.WPF
 		/// <exception cref="T:System.Security.SecurityException">
 		/// The caller does not have the required permission.
 		///   </exception>
-		[SecurityPermissionAttribute(SecurityAction.Demand, SerializationFormatter = true)]
+		[SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
 		public virtual void GetObjectData(SerializationInfo info, StreamingContext context)
 		{
 			info.AddValue("IsDirty", _IsDirty);

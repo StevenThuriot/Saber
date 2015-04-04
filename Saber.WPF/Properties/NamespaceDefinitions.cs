311f40b776825f3925a9e49555bf952cbe1dc753
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2015-04-04 15:57:23 +0200

    Fixed namespaces

--- Saber.WPF/Properties/NamespaceDefinitions.cs
+++ Saber.WPF/Properties/NamespaceDefinitions.cs
@@ -1,2 +1,4 @@
-[assembly: XmlnsDefinition("http://moon.thuriot.be/WPF", "Saber.WPF")]
-[assembly: XmlnsDefinition("http://moon.thuriot.be/WPF/Converters", "Saber.WPF.Converters")]
+using System.Windows.Markup;
+
+[assembly: XmlnsDefinition("http://saber.thuriot.be/WPF", "Saber.WPF")]
+[assembly: XmlnsDefinition("http://saber.thuriot.be/WPF/Converters", "Saber.WPF.Converters")]
